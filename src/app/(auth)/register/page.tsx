f08edcd685875b7ac0637589090f603e57dea419
gustaaxz <gustavoooschmitt@gmail.com>
2026-07-21 09:07:31 -0300

    feat: add registration page with authentication form layout

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -6,16 +6,27 @@ import { Separator } from "@/components/ui/separator";
 
 export default function RegisterPage() {
     return (
-        <main className="flex h-screen items-center justify-center">
-            <Card>
-                <CardHeader className="text-center">
-                    <CardTitle>Registro</CardTitle>
-                </CardHeader>
+        <main className="flex h-screen w-full">
+            {/* Coluna da Esquerda */}
+            <div className="flex-1 bg-slate-100" 
+            
+            />
 
-                <CardContent className="flex justify-center">
-                    <Button>Registrar-se</Button>
-                </CardContent>
-            </Card>
+            {/* Coluna da Direita (com o Card) */}
+            <div className="flex flex-1 items-center justify-center bg-white p-4">
+                <Card className="w-full max-w-[400px] shadow-lg">
+                    <CardHeader className="text-center">
+                        <CardTitle>Registro</CardTitle>
+                    </CardHeader>
+
+                    <CardContent className="flex flex-col gap-4 justify-center">
+                        <Input placeholder="Digite seu e-mail" />
+                        <Input placeholder="Digite sua senha" type="password" className="align=inline-end"/>
+                        <Input placeholder="Confirme sua senha" type="password" />
+                        <Button>Registrar-se</Button>
+                    </CardContent>
+                </Card>
+            </div>
         </main>
     )
 }
