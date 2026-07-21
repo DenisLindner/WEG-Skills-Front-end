7be2099cc9e17d055f8398d6d40e9a70be841a5b
gustaaxz <gustavoooschmitt@gmail.com>
2026-07-21 12:02:44 -0300

    feat: finished registration page and added weg brand svgs and swapped font to Inter

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,26 +4,104 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
+import Image from "next/image";
+
 export default function RegisterPage() {
     return (
         <main className="flex h-screen w-full">
-            {/* Coluna da Esquerda */}
-            <div className="flex-1 bg-slate-100" 
-            
-            />
+            {/* Coluna da Esquerda (com imagem da fábrica, overlay e Logo) */}
+            <div className="relative w-[60%] bg-[url('/assets/images/fabric.png')] bg-cover bg-center flex items-center justify-center">
+                {/* Overlay preto com 50% de opacidade */}
+                <div className="absolute inset-0 bg-black/50" />
+
+                {/* Logo (z-10 para ficar por cima do overlay preto) */}
+                <div className="z-10">
+                    <Image
+                        src="/assets/icons/Logo WEG Skills Branca.svg"
+                        alt="WEG Skills Logo"
+                        width={575}
+                        height={100}
+                        priority
+                    />
+                </div>
+            </div>
 
             {/* Coluna da Direita (com o Card) */}
-            <div className="flex flex-1 items-center justify-center bg-white p-4">
-                <Card className="w-full max-w-[400px] shadow-lg">
-                    <CardHeader className="text-center">
-                        <CardTitle>Registro</CardTitle>
+            <div className="flex w-[45%] items-center justify-center p-4 bg-[#005294]">
+                <Card className="w-full max-w-[500px] border-none p-6 sm:p-12 shadow-2xl rounded-2xl">
+                    <CardHeader className="flex flex-col items-center gap-6 pb-5 text-center">
+                        <CardTitle className="text-[40px] sm:text-[42px] font-light text-[#005294] tracking-wide">
+                            REGISTRO
+                        </CardTitle>
+
+                        {/* Linha divisória de Registro para Email */}
+                        <div className="h-0.5 w-full max-w-[80%] rounded-full bg-[#005294] mb-3" />
                     </CardHeader>
 
-                    <CardContent className="flex flex-col gap-4 justify-center">
-                        <Input placeholder="Digite seu e-mail" />
-                        <Input placeholder="Digite sua senha" type="password" className="align=inline-end"/>
-                        <Input placeholder="Confirme sua senha" type="password" />
-                        <Button>Registrar-se</Button>
+                    {/* Div 1: Email */}
+
+                    <CardContent className="flex flex-col gap-8">
+                        <div className="group relative flex flex-col pt-6 w-[85%] mx-auto">
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="exemplo@gmail.com"
+                                className="bg-[#FFFFFF] peer rounded-none border-0 border-b-2 border-slate-200 px-2 py-2 text-base shadow-none focus-visible:border-[#005294] focus-visible:ring-0 placeholder:text-base"
+                            />
+
+                            <Label
+                                htmlFor="email"
+                                className="absolute pl-2 top-0 text-base font-normal text-slate-400 transition-colors peer-focus:text-[#005294] group-hover:text-[#005294]"
+                            >
+                                Email
+                            </Label>
+                        </div>
+
+                        {/* Div 2: Senha */}
+
+                        <div className="group relative flex flex-col pt-6 w-[85%] mx-auto">
+                            <Input
+                                id="password"
+                                type="password"
+                                placeholder="************"
+                                className="bg-[#FFFFFF] peer rounded-none border-0 border-b-2 border-slate-200 px-2 py-2 text-base shadow-none focus-visible:border-[#005294] focus-visible:ring-0 placeholder:translate-y-1 placeholder:text-base"
+                            />
+
+                            <Label
+                                htmlFor="password"
+                                className="absolute pl-2 top-0 text-base font-normal text-slate-400 transition-colors peer-focus:text-[#005294] group-hover:text-[#005294]"
+                            >
+                                Senha
+                            </Label>
+                        </div>
+
+                        {/* Div 3: Confirmar Senha */}
+
+                        <div className="group relative flex flex-col pt-6 w-[85%] mx-auto">
+                            <Input
+                                id="confirm-password"
+                                type="password"
+                                placeholder="************"
+                                className="bg-[#FFFFFF] peer rounded-none border-0 border-b-2 border-slate-200 px-2 py-2 text-base shadow-none focus-visible:border-[#005294] focus-visible:ring-0 placeholder:translate-y-1 placeholder:text-base"
+                            />
+
+                            <Label
+                                htmlFor="confirm-password"
+                                className="absolute pl-2 top-0 text-base font-normal text-slate-400 transition-colors peer-focus:text-[#005294] group-hover:text-[#005294]"
+                            >
+                                Confirmar Senha
+                            </Label>
+                        </div>
+
+                        {/* Div 4: Botão Registrar-se */}
+
+                        <div>
+                            <div className="mt-6 flex justify-center">
+                                <Button className="rounded-lg bg-[#005294] px-10 py-4.5 text-base font-medium text-white hover:bg-[#003d70]">
+                                    Registrar-se
+                                </Button>
+                            </div>
+                        </div>
                     </CardContent>
                 </Card>
             </div>
