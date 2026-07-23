97185e82fd30f6543aaf62238255a04afabab97d
add095 <emanuelfilipi095@gmail.com>
2026-07-23 14:21:40 -0300

    feature: bestCourses adding

---
+++ src/app/(general)/teste/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/molecules/header"
+import BestCourses from "@/components/organisms/bestCourses";
+export default function Home() {
+  return (
+   <>
+    <BestCourses/>
+    </> 
+  );
+}
