9f3fd0eb5d19240eeaea125382a3fec431e149d0
MatheusEich15 <matheus_eichendorf@estudante.sesisenai.org.br>
2026-07-23 08:31:22 -0300

    fix: separate package location

---
+++ src/app/(general)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Header from "@/components/molecules/header";
+
+const interSans = Inter({
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "WEG Skills",
+  description: "Buildt by CentroWEG",
+};
+
+export default function RootLayout({children}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="pt-BR" className={`${interSans.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <Header/>
+        {children}
+
+      </body>
+    </html>
+  );
+
+}
