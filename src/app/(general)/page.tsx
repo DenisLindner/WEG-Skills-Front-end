505bf120589e6c865350712abb0b4da6a8ec329e
MatheusEich15 <matheus_eichendorf@estudante.sesisenai.org.br>
2026-07-23 10:21:02 -0300

    feat: example courses added in landing page

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import CourseCard from "@/components/molecules/course-card";
 import InfoCarousel from "@/components/molecules/InfoCarousel";
+
 export default function Home() {
   return (
-   <>
-    <InfoCarousel/> 
-    </> 
+    <>
+      <InfoCarousel />
+      <div className="p-4 flex gap-4 flex-wrap">
+        {/* Exemplo de card com nota 2/10 (equivale a 1 estrela preenchida) */}
+        <CourseCard
+          title="Ferramentaria para Sistemas WEB"
+          description="Uma breve descrição sobre"
+          rating={2}
+          buttonText="Matricular"
+        />
+
+        {/* Exemplo de card com nota 8/10 (equivale a 4 estrelas preenchidas) */}
+        <CourseCard
+          title="Desenvolvimento React & Next.js"
+          description="Aprenda a criar aplicações modernas"
+          rating={8}
+          buttonText="Matricular"
+        />
+      </div>
+    </>
   );
 }
