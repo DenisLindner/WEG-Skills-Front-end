ffadd73a3a31772afea93da1fc6f94d4d52a30d5
MatheusEich15 <MatheusEich15@users.noreply.github.com>
2026-07-28 12:59:04 -0300

    fix: card accept  diferent icons, avoiding errors

--- src/components/atoms/cardInfo.tsx
+++ src/components/atoms/cardInfo.tsx
@@ -1,13 +1,17 @@
 import { LucideIcon } from "lucide-react"
 
-export default function InfoCard({ Card }: { Card: { icon: LucideIcon; titulo: string; subtitulo: string; dados: string; alt?: string } }) {
+export default function InfoCard({ Card }: { Card: { icon: LucideIcon | string; titulo: string; subtitulo: string; dados: string; alt?: string } }) {
     const Icon = Card.icon
     return (
         <div className="bg-[#f0f0f0] shadow-[-10px_10px_0px_#00579D] w-[225px] h-[275px] ml-3 mb-6 rounded-[6px] shrink-0">
             <div className="h-[100%] flex justify-between items-center flex-col pb-[35px]">
                 <div className="h-[100%] flex justify-end items-center flex-col pb-[15px] ">
                     <div className="rounded-[100px] w-[70px] h-[70px] bg-[#00579D] flex justify-center items-center mb-[15px]">
-                        <Icon className="w-10 h-10 text-white" />
+                        {typeof Icon === "string" ? (
+                            <img src={Icon} alt={Card.alt || Card.titulo} className="w-10 h-10 filter invert brightness-200" />
+                        ) : (
+                            <Icon className="w-10 h-10 text-white" />
+                        )}
                     </div>
                     <h3 className="text-[#808080] text-[20px] text-center ">{Card.titulo}</h3>
                     <h3 className="text-[#808080] text-[20px] text-center ">{Card.subtitulo}</h3>
