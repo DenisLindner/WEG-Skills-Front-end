4c7e32491e8b9595106f3d6c2b70d14727d5307e
add095 <emanuelfilipi095@gmail.com>
2026-07-30 08:18:26 -0300

    fix: fix the size of cards

--- src/app/(general)/about/page.tsx
+++ src/app/(general)/about/page.tsx
@@ -4,9 +4,9 @@ import Hero from "@/components/organisms/hero";
 
 export default function AboutPage() {
   return (
-    <main className="w-full flex flex-col items-center">
+    <main className="w-full flex flex-col items-center overflow-hidden">
       <Hero page="about" />
-      <div className="w-full max-w-[90%] mx-auto flex flex-col items-center gap-8 py-6">
+      <div className="w-full max-w-[90%] mx-auto flex flex-col items-center gap-8 py-6 ">
         <CardAbout />
         <InfoCardAbout />
       </div>
