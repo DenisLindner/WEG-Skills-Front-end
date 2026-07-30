d2c388beb57f363b43ae4e3ef3a1c689cd806aec
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-30 18:45:38 -0300

    fix: code format fixed in certificate service

--- src/services/certificate.service.ts
+++ src/services/certificate.service.ts
@@ -2,8 +2,9 @@ import {backendFetch} from "@/services/backend.service";
 import {Certificate} from "@/types/certificate/certificate";
 
 export const certificateService = {
-    validate: (code: string) =>
-        backendFetch<Certificate>(`/certificates/validate/${encodeURIComponent(code)}`, {
+    validate: (code: string) => {
+        return backendFetch<Certificate>(`/certificates/validate/${encodeURIComponent(code)}`, {
             auth: false
-        })
+        });
+    }
 }
