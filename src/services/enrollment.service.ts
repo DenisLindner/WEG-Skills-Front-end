b242a434575a3d1a3f402aa5c92da2750e88ecc5
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-31 08:54:19 -0300

    fix: enrollment service paths fixed

--- src/services/enrollment.service.ts
+++ src/services/enrollment.service.ts
@@ -4,12 +4,12 @@ import {SpringPage} from "@/types/common/spring-page";
 
 export const enrollmentService = {
     enroll: (courseId: number) => {
-        return backendFetch<Enrollment>('/enrollments', {
+        return backendFetch<Enrollment>('enrollments', {
             method: 'POST',
             body: JSON.stringify({courseId})
         });
     },
     mineEnrollment: (page = 0, size = 100) => {
-        return backendFetch<SpringPage<Enrollment>>(`/enrollments/me?${toQuery({page, size})}`);
+        return backendFetch<SpringPage<Enrollment>>(`enrollments/me?${toQuery({page, size})}`);
     }
 }
