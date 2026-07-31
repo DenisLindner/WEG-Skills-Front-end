31af33bea768a8fe2a7b2f901eed82173a6ee919
add095 <emanuelfilipi095@gmail.com>
2026-07-31 10:10:40 -0300

    fix: fix the import of media

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -2,12 +2,11 @@ import "server-only";
 
 import type {Media} from "@/types/media/media";
 import {backendFetch} from "./backend.service";
-
-export type MediaTarget = "course" | "module" | "lesson" | "me";
+import { MediaTarget } from "@/types/media/media-target";
 
 export const mediaService = {
     complete : (mediaId: number, target: MediaTarget, targetId?: number) => {
-        const suffix = target === "me" ? "/me/complete" : `/${target}/${target}/${targetId}/complete`
-        return backendFetch<Media> (`/medias/${mediaId}${suffix}`, {method: "POST"})
+        const suffix = target === "me" ? "me/complete" : `${target}/${target}/${targetId}/complete`
+        return backendFetch<Media> (`medias/${mediaId}${suffix}`, {method: "POST"})
     },
 }
