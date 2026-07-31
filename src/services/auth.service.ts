405928c3742bf2e10838661ea7ab9e99875b2885
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-31 08:53:36 -0300

    fix: auth service paths fixed and return type added

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,17 +1,18 @@
 import {LoginInput} from "@/types/auth/login-input";
 import {backendFetch} from "@/services/backend.service";
 import {RegisterInput} from "@/types/auth/register-input";
+import {AuthResponse} from "@/types/auth/auth-response";
 
 export const authService = {
     login: (input: LoginInput) => {
-        return backendFetch('/auth/login', {
+        return backendFetch<AuthResponse>('auth/login', {
             method: 'POST',
             body: JSON.stringify(input),
             auth: false,
         });
     },
     register: (input: RegisterInput) => {
-        return backendFetch('/auth/register', {
+        return backendFetch<AuthResponse>('auth/register', {
             method: 'POST',
             body: JSON.stringify(input),
             auth: false,
