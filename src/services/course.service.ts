1ab03ee0db9ec0bc7a6ee0b9547822ffae8b5772
Denis Lindner <lindnerdenis19@gmail.com>
2026-07-31 08:54:03 -0300

    fix: course service paths fixed

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -8,10 +8,11 @@ import {CourseInput} from "@/types/course/course-input";
 import {Certificate} from "@/types/certificate/certificate";
 import {MediaUploadInput} from "@/types/media/media-upload-input";
 import {UploadTicket} from "@/types/media/upload-ticket";
+import {CourseProgress} from "@/types/course/course-progress";
 
 export const courseService = {
     listCoursesPublished: (params: ListParams = {}) => {
-        const base = params.title ? '/courses/title' : '/courses';
+        const base = params.title ? 'courses/title?' : 'courses?';
         const query = toQuery({
             title: params.title,
             page: params.page ?? 0,
@@ -20,7 +21,7 @@ export const courseService = {
         return backendFetch<SpringPage<Course>>(`${base}${query}`);
     },
     listCoursesPrivate: (params: ListParams = {}) => {
-        const base = params.title ? '/courses/private/title' : '/courses/private';
+        const base = params.title ? 'courses/private/title?' : 'courses/private?';
         const query = toQuery({
             title: params.title,
             page: params.page ?? 0,
@@ -29,46 +30,46 @@ export const courseService = {
         return backendFetch<SpringPage<Course>>(`${base}${query}`);
     },
     listCoursesAdmin: (params: ListParams = {}) => {
-        return backendFetch<SpringPage<Course>>(`/courses/admin?${pageQuery(params)}`);
+        return backendFetch<SpringPage<Course>>(`courses/admin?${pageQuery(params)}`);
     },
     topCourses: () => {
-        return backendFetch<CourseWithRating[]>(`/courses/top-courses`, {auth: false});
+        return backendFetch<CourseWithRating[]>(`courses/top-courses`, {auth: false});
     },
     findCourseById: (id: number) => {
-        return backendFetch<Course>(`/courses/${id}`);
+        return backendFetch<Course>(`courses/${id}`);
     },
     progressCourse: (id: number) => {
-        return backendFetch(`/courses/${id}/progress/me`);
+        return backendFetch<CourseProgress>(`courses/${id}/progress/me`);
     },
     createCourse: (input: CourseInput) => {
-        return backendFetch<Course>(`/courses`, {
+        return backendFetch<Course>(`courses`, {
             method: 'POST',
             body: JSON.stringify(input)
         });
     },
     updateCourse: (id: number, input: Partial<CourseInput>) => {
-        return backendFetch<Course>(`/courses/${id}`, {
+        return backendFetch<Course>(`courses/${id}`, {
             method: 'PATCH',
             body: JSON.stringify(input)
         });
     },
     removeCourse: (id: number) => {
-        return backendFetch<void>(`/courses/${id}`, {
+        return backendFetch<void>(`courses/${id}`, {
             method: 'DELETE'
         });
     },
     publishCourse: (id: number) => {
-        return backendFetch<Course>(`/courses/${id}/publish`, {
+        return backendFetch<Course>(`courses/${id}/publish`, {
             method: 'PATCH'
         });
     },
     certificateCourse: (id: number) => {
-        return backendFetch<Certificate>(`/courses/${id}/certificate`, {
+        return backendFetch<Certificate>(`courses/${id}/certificate`, {
             method: 'PUT'
         });
     },
     createImageUpload: (id: number, input: MediaUploadInput) => {
-        return backendFetch<UploadTicket>(`/courses/${id}/images/upload`, {
+        return backendFetch<UploadTicket>(`courses/${id}/images/upload`, {
             method: 'POST',
             body: JSON.stringify(input)
         });
