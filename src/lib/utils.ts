87edafe83346919322c63eb7564ffb5aa76b8749
Denis Lindner <lindnerdenis19@gmail.com>
2026-08-01 21:37:16 -0300

    fix: utils lib fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,6 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import {ListParams} from "@/types/common/list-params";
 import {toQuery} from "@/services/backend.service";
-import {RequestValidationError} from "@/lib/response";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -16,7 +15,7 @@ export function pagination(searchParams: URLSearchParams) {
   const page = Number(searchParams.get("page") ?? 0)
   const size = Number(searchParams.get("size") ?? 12)
   if (!Number.isSafeInteger(page) || page < 0 || !Number.isSafeInteger(size) || size < 1 || size > 100) {
-    throw new RequestValidationError("Paginação inválida.")
+    // throw new RequestValidationError("Paginação inválida.")
   }
   return { page, size }
 }
