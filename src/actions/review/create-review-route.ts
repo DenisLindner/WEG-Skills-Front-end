11330d9e87866dc363ce50c24d44085e1f6ed398
Denis Lindner <lindnerdenis19@gmail.com>
2026-08-02 23:07:03 -0300

    fix: create review action fixed

--- src/actions/review/create-review-route.ts
+++
@@ -1,8 +0,0 @@
-import {reviewService} from "@/services/review.service";
-import {apiResponse} from "@/lib/response";
-
-export async function createReviewRoute(courseId: number, rate: number) {
-    return apiResponse(() => {
-        return reviewService.createReview(courseId, rate);
-    })
-}
