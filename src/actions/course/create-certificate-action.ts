b56c52bf0fd4f74972a04bd5df4a5af27574c1d2
Denis Lindner <lindnerdenis19@gmail.com>
2026-08-04 14:29:46 -0300

    fix: find lesson and create certificate actions fixed

--- src/actions/course/create-certificate-action.ts
+++ src/actions/course/create-certificate-action.ts
@@ -1,4 +1,4 @@
-"use server";
+"use server"
 
 import {getSession} from "@/lib/session";
 import {ApiError} from "@/services/api-error";
--- src/actions/lesson/find-lesson-by-id-route.ts
+++
@@ -1,8 +0,0 @@
-import {apiResponse} from "@/lib/response";
-import {lessonService} from "@/services/lesson.service";
-
-export async function findLessonByIdRoute(id: number) {
-    return apiResponse(() => {
-        return lessonService.findById(id);
-    })
-}
