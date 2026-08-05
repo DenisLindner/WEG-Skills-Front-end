9be50718dc9754699c6c92d10aee429252924ea2
Denis Lindner <lindnerdenis19@gmail.com>
2026-08-05 17:27:54 -0300

    fix: review service fixed

--- src/services/review.service.ts
+++ src/services/review.service.ts
@@ -9,24 +9,10 @@ export const reviewService = {
             `reviews/${courseId}?${toQuery({ page, size })}`,
         );
     },
-    mineReviews: (page = 0, size = 100) => {
-        return backendFetch<SpringPage<Review>>(`reviews/me?${toQuery({ page, size })}`);
-    },
     createReview: (courseId: number, rate: number) => {
         return backendFetch<Review>('reviews', {
             method: 'POST',
             body: JSON.stringify({ courseId, rate })
         });
     },
-    updateReview: (id: number, rate: number) => {
-        return backendFetch<Review>(`reviews/${id}`, {
-            method: 'PATCH',
-            body: JSON.stringify({ rate })
-        });
-    },
-    removeReview: (id: number) => {
-        return backendFetch<void>(`reviews/${id}`, {
-            method: 'DELETE'
-        });
-    }
 }
